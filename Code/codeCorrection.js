259e189ed80fe49ae6c25a39b860d16ab88e6357
vkatvell <venkatvellankiphoto@gmail.com>
2023-12-05 22:31:57 -0600

    code correction trial

--- Code/codeCorrection.js
+++ Code/codeCorrection.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const correctedJsonFilePath = "./Data/corrected_code.json";
+
 // Reads JSON data from a file
 function readJsonFile(filePath) {
     const data = fs.readFileSync(filePath, 'utf8');
@@ -13,22 +15,38 @@ function writeFile(filePath, data) {
 
 // Main function to apply corrections
 function applyCorrections() {
-    const corrections = readJsonFile('corrected_code.json');
+    const corrections = readJsonFile(correctedJsonFilePath);
 
-    corrections.forEach(correction => {
-        for (const filePath in correction) {
-            let fileData = fs.readFileSync(filePath, 'utf8');
-            const lines = fileData.split('\n');
+    for (const filePath in corrections) {
+        let fileData = fs.readFileSync(filePath, 'utf8');
+        let lines = fileData.split('\n');
 
-            correction[filePath].forEach(change => {
-                const lineNumber = change.lineNumber - 1; // Array is 0-indexed
-                lines[lineNumber] = change.correctedCodeSnippet.join('\n');
-            });
+        corrections[filePath].forEach(change => {
+            const lineNumber = change.lineNumber - 1; // 0-based index
+            const columnNumber = change.columnNumber - 1; // 0-based index
 
-            fileData = lines.join('\n');
-            writeFile(filePath, fileData);
-        }
-    });
+            const originalLine = lines[lineNumber];
+            const correctedLine = change.correctedCodeSnippet.join('\n');
+            
+            // Replace the specific part of the line based on the column number
+            if (originalLine.trim() !== correctedLine.trim()) {
+                const beforeError = originalLine.substring(0, columnNumber);
+                const afterError = originalLine.substring(columnNumber);
+
+                // Find the position in the corrected line to start the replacement
+                const replacementStartIndex = correctedLine.indexOf(afterError);
+                const replacement = correctedLine.substring(0, replacementStartIndex);
+
+                lines[lineNumber] = beforeError + replacement + afterError;
+            } else {
+                // If the entire line is a corrected snippet, replace the line
+                lines[lineNumber] = correctedLine;
+            }
+        });
+
+        fileData = lines.join('\n');
+        writeFile(filePath, fileData);
+    }
 
     console.log('Corrections applied.');
 }
