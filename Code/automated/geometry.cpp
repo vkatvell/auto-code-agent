d33a309796e6ec0c5719b8f806c340db070edea8
vkatvell <venkatvellankiphoto@gmail.com>
2023-12-08 18:32:09 -0600

    geometry test file

--- Code/automated/geometry.cpp
+++ Code/automated/geometry.cpp
@@ -2,3 +2,12 @@ double areaCircle(double radius)
 {
     double area = 3.14 * radius * radius return area
 }
+
+int main(void)
+{
+    double radius = 21.0;
+    double area = areaCircle(radus);
+
+    std::cout << "The area of circle with radius " << radius << " is: " << area << std::endl;
+    return 0;
+}
